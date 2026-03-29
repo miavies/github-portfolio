a9639b064d621a1c58412d302031556baa63f2eb
miavies <michaelasantiago25@gmail.com>
2026-03-29 18:12:47 +0800

    fixed logic error

--- script.js
+++ script.js
@@ -14,13 +14,15 @@ function plusSlides(n, no) {
 function showSlides(n, no) {
   let i;
   let x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}    
-  if (n < 2) {slideIndex[no] = x.length}
-  
+
+  if (n > x.length) { slideIndex[no] = 1; }    
+  if (n < 1) { slideIndex[no] = x.length; }
+
   for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
+    x[i].style.display = "none";  
   }
-  x[slideIndex[no]-1].style.display = "block";  
+
+  x[slideIndex[no] - 1].style.display = "block";  
 }
 
 
