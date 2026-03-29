fa2d50b44dc6f766f6110a0b1fd8b93eb0f1a4c7
miavies <michaelasantiago25@gmail.com>
2026-03-29 18:14:56 +0800

    fixed backward cycle

--- script.js
+++ script.js
@@ -8,21 +8,23 @@ showSlides(1, 4);
 showSlides(1, 5);
 
 function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
+  showSlides(slideIndex[no] + n, no);
 }
 
 function showSlides(n, no) {
   let i;
   let x = document.getElementsByClassName(slideId[no]);
 
-  if (n > x.length) { slideIndex[no] = 1; }    
-  if (n < 1) { slideIndex[no] = x.length; }
+  if (n > x.length) { n = 1; }
+  if (n < 1) { n = x.length; }
+
+  slideIndex[no] = n; // ✅ update ONLY here
 
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
   }
 
-  x[slideIndex[no] - 1].style.display = "block";  
+  x[n - 1].style.display = "block";  
 }
 
 
