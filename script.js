b5becb5f962dcc6c278e4af2c6c1ad50a5d20684
miavies <michaelasantiago25@gmail.com>
2026-03-29 18:22:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 let slideIndex = [1, 1, 1, 1, 1, 1];
 let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6"];
 
-// Initialize all slideshows properly
 window.onload = function () {
   let containers = document.getElementsByClassName("slideshow-container");
 
   for (let no = 0; no < containers.length; no++) {
     let slides = containers[no].getElementsByClassName(slideId[no]);
 
-    // Hide all slides
     for (let i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
     }
 
-    // Show first slide
     if (slides.length > 0) {
       slides[0].style.display = "block";
       slideIndex[no] = 1;
@@ -31,11 +28,9 @@ function plusSlides(n, no, btn) {
   if (slideIndex[no] > slides.length) slideIndex[no] = 1;
   if (slideIndex[no] < 1) slideIndex[no] = slides.length;
 
-  // Hide all slides in THIS container only
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
 
-  // Show current slide
   slides[slideIndex[no] - 1].style.display = "block";
 }
